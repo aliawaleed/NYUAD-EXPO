7861751a20387dcfc6a2efeb0a8bf3f4e79840cf
aliawaleed <57350259+aliawaleed@users.noreply.github.com>
2022-04-01 23:48:44 +0400

    updated landing page code and js

--- public/landingPage.js
+++ public/landingPage.js
@@ -5,12 +5,26 @@ window.addEventListener('load', () => {
       e.preventDefault();
       let name = document.getElementById('name-input').value;
       let room = document.getElementById('room-input').value;
-      console.log(name, room);
       //save the name and the room in session storage
       sessionStorage.setItem('name', name);
       sessionStorage.setItem('room', room);
 
       //redirect the user to game.html
-      window.location = '/game.html';
+      console.log(room)
+      if (room == 'Field') {
+        window.location = '/field/field.html';
+      }
+      else if (room == 'A2'){
+        window.location = '/a2/a2.html';
+      }
+      else if (room == 'C2'){
+        window.location = '/c2/c2.html';
+      }
+      else if (room == 'D2'){
+        window.location = '/d2/d2.html';
+      }
+      else{ //to be changed
+        alert("not available")
+      }
     })
 })
