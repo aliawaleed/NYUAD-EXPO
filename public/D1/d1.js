47391297d74cfbf1571d8f749fc1a3c5a501e2e1
Soojin Lee <sl7362@nyu.edu>
2022-05-02 15:59:37 +0400

    github

--- public/D1/d1.js
+++ public/D1/d1.js
@@ -4,36 +4,100 @@ let roomStart = 'D1'
 let Player1Instruction = "You will start with Acting"
 let Player2Instruction = "You will start with Guessing"
 
+// This is a test of the p5LiveMedia webrtc library and associated service.
+// Open this sketch up 2 times to send video back and forth
 
-
-let video;
-let ovideo;
+let myVideo;
 let otherVideo;
+let getwordButton;
+let drawthis;
 
-//p5.js Canvas for video Camera
+
 function setup() {
-    let canvas = createCanvas(windowWidth, windowHeight * 0.6);
-    canvas.parent('sketch-canvas');
-    video = createCapture(VIDEO, function(stream) {
-        p5l = new p5Live(this,"CAPTURE",stream)
-        p5l.on('stream', gotStream);
-    });  
-    video.muted = true;     
-	video.hide();
 
- }
- 
- function draw() {
-    image(video,0,0,width/2,height);
-    if (ovideo != null) {
-        image(ovideo,width/2,0,width,height);
-    }
+    let canvas = createCanvas(windowWidth*0.6, windowHeight * 0.6);
+    canvas.parent('sketch-canvas');
+  
+    myVideo = createCapture(VIDEO, 
+      function(stream) {
+        let p5l = new p5LiveMedia(this, "CAPTURE", stream, "charades");
+        p5l.on('stream', gotStream);
+      }
+    );  
+    myVideo.muted = true;     
+    myVideo.hide();
+}
+
+function draw() {
+  background(220);
+  stroke(255);
+  if (myVideo != null) {
+    image(myVideo,0,0,width/2,height);
+    text("My Video", 10, 10);
+  }
+  ellipse(mouseX,mouseY,100,100);
+
+  if (otherVideo != null) {
     image(otherVideo,width/2,0,width/2,height);
-}		
+    text("Their Video", width/2+10, 10);
+  }  
+}
 
 // We got a new stream!
-function gotStream(stream) {
-    // This is just like a video/stream from createCapture(VIDEO)
-    ovideo = stream;
-    //ovideo.hide();
+function gotStream(stream, id) {
+  // This is just like a video/stream from createCapture(VIDEO)
+  otherVideo = stream;
+  //otherVideo.id and id are the same and unique identifiers
+  otherVideo.hide();
 }
+
+
+window.addEventListener("load", () => { 
+  getwordButton = document.getElementById('getword-button');
+  drawthis = document.getElementById("generated");
+})
+
+function generateWord(){
+  console.log('click');
+  fetch('word.json')
+     .then(response => response.json())
+     .then(data => {
+        console.log(data);
+        //Do something with 'data'
+        let wordlist = data.words;
+        let randomItem = wordlist[Math.floor(Math.random() * wordlist.length)];
+        console.log(randomItem);
+
+        //send the data to update for all
+        socket.emit('generatedword','');
+        console.log("this"+ randomItem);
+     });
+};
+
+// Listen for word named 'displayrandomword' from the server - only applied to the drawer
+socket.on('displayrandomword', function (data) {
+   console.log("displayrandomword arrived");
+   console.log(data);
+
+   //Create a message string and page element
+   let receiveword = data;
+   currentword = receiveword;
+   drawthis.innerHTML = receiveword;
+
+   //A perosn who draws can't guess the word
+   sendButton.disabled = true;
+   msgInput.disabled = true;
+   sendButton.style.opacity(0.6);
+});
+
+
+socket.on('drawclicked', function (data) {
+  console.log("drawclicked");
+});
+
+
+function startTimer() {
+  console.log('startTimer');
+  generateWord();
+}
+
--- public/helper.js
+++ public/helper.js
@@ -1,16 +1,17 @@
 let socket = io();
-//console.log("helper is conected");
+console.log("helper is conected");
 
 //listen for confirmation of socket; confirms that the client is connected
 socket.on('connect', () => {
    console.log("client connected via sockets");
    // now that client has connected to server, emit name and room information
    let data = {
-      'name': sessionStorage.getItem('name'),
-      'room': sessionStorage.getItem('room'),
+       'name': sessionStorage.getItem('name'),
+       'room': sessionStorage.getItem('room'),
    }
    socket.emit('userData', data);
 })
+
 
 let allow_start;
 let game;
